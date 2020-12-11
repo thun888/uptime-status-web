5c04b50a9c7b62d09088296497bdec27b7a2af1b
thun888 <2238342947@qq.com>
2020-12-11 19:58:50 +0800

    f
    f

--- config.js
+++ config.js
@@ -2,37 +2,38 @@
 window.Config = {
 
   // 站点名
-  SiteName: 'Uptime Status',
+  SiteName: 'thun888的服务器们',
 
   // 站点链接
-  SiteUrl: '/',
+  SiteUrl: 'http://stats.thun888.xyz',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur1070350-3cfc2387e30ddf85dad15f87',
   ],
 
   // 是否显示监测站点的链接
-  ShowLink: true,
+  ShowLink: false,
 
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 60,
+  CountDays: 30,
 
   // 导航栏菜单
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://status.org.cn/'
+      url: 'http://server.thun888.xyz'
     },
     {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      text: 'BiliBili',
+      url: 'https://space.bilibili.com/451090261'
+    },
+    {
+      text: '附监控站',
+      url: 'http://server-9g1wx3kb731de358-1302025169.tcloudbaseapp.com'
     }
   ]
 };
